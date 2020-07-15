75df577ec1b98c8866bf01069dd0b9fb19a6810e
Scott Morin <scottanthonymorin@gmail.com>
2020-07-15 10:26:11 -0400

    clean up server.js

--- server.js
+++ server.js
@@ -5,29 +5,21 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const { flights } = require('./test-data/flightSeating');
 
-const PORT = process.env.PORT || 8000;
-
-const handleFlight = (req, res) => {
-    const { flightNumber } = req.params;
-    // get all flight numbers
-    const allFlights = Object.keys(flights);
-    // is flightNumber in the array?
-    console.log('REAL FLIGHT: ', allFlights.includes(flightNumber))
-}
-
-{name: 'Fred'}
 express()
-    .use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        next();
-    })
-	.use(morgan('dev'))
-	.use(express.static('public'))
-    .use(bodyParser.json())
-    .use(express.urlencoded({extended: false}))
-    
-    // endpoints
-    .get('/flights/:flightNumber', handleFlight)
-    .use((req, res) => res.send('Not Found'))
-    .listen(PORT, () => console.log(`Listening on port ${PORT}`));
+  .use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+  })
+  .use(morgan('dev'))
+  .use(express.static('public'))
+  .use(bodyParser.json())
+  .use(express.urlencoded({ extended: false }))
+
+  // endpoints
+
+  .use((req, res) => res.send('Not Found'))
+  .listen(8000, () => console.log(`Listening on port 8000`));
